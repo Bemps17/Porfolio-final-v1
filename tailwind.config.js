270270e3387be712a3307859e98c98a3df6109b9
Bemps17 <bertrandfouquet@gmail.com>
2025-08-31 02:19:10 +0200

    tagmanager

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./assets/**/*.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
